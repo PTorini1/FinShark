287c4f98deca71acc141a2cf03b463cc78fc8dc1
PTorini1 <pedrotorini123@gmail.com>
2024-04-04 15:27:05 -0300

    feat: implementing validations

--- api/Dtos/Stock/CreateStockRequestDto.cs
+++ api/Dtos/Stock/CreateStockRequestDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,11 +9,28 @@ namespace api.Dtos.Stock
 {
     public class CreateStockRequestDto
     {
+        [Required]
+        [MaxLength(10, ErrorMessage = "Symbol cannoty be over 10 characters")]
         public string Symbol { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(20, ErrorMessage = "Symbol cannoty be over 20 characters")]
         public string CompanyName { get; set; } = string.Empty;
+
+        [Required]
+        [Range(1, 1000000000000)]
         public decimal Purchase { get; set; }
+
+        [Required]
+        [Range(0.001, 100)]
         public decimal LastDiv { get; set; }
+
+        [Required]
+        [MaxLength(20, ErrorMessage = "Industry cannoty be over 20 characters")]
         public string Industry { get; set; } = string.Empty;
+
+        [Required]
+        [Range(1, 5000000000000)]
         public long MarketCap { get; set; }
     }
 }
